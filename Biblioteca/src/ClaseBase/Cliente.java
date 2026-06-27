174de9d2e8f2412f437ba05a4ae1fcc2094aead5
ivyhb <u24323982@utp.edu.pe>
2026-06-27 10:40:37 -0500

    Moficación clase Cliente

--- Biblioteca/src/ClaseBase/Cliente.java
+++ Biblioteca/src/ClaseBase/Cliente.java
@@ -6,7 +6,40 @@ public class Cliente {
     private String dni;
     private String primerNombre;
     private String primerApellido;
-    private String telefono; 
+    private String telefono;
     private String correo;
-    private LocalDate fechaRegistro; 
+    private LocalDate fechaRegistro;
+
+    public Cliente(String dni, String primerNombre, String primerApellido, String telefono, String correo) {
+        this.dni = dni;
+        this.primerNombre = primerNombre;
+        this.primerApellido = primerApellido;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.fechaRegistro = LocalDate.now();
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
 }
